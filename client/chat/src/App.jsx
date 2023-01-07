ee96319762d6ce2710452b7dc236bbee3c1e5116
AgustinMillan <victor.agustin.millan@gmail.com>
2023-01-07 18:52:12 -0300

    function message

--- client/chat/src/App.jsx
+++ client/chat/src/App.jsx
@@ -1,11 +1,26 @@
-import { useState } from 'react'
 import './App.css'
+import { io } from "socket.io-client";
+import { useState } from 'react';
+import { useEffect } from 'react';
 
+const socket = io("http://localhost:3000");
 function App() {
-
+  const [message,setMessage] = useState("")
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    socket.emit("messageOfClient", message)
+    setMessage("");
+  }
+  useEffect(()=>{
+    const recibM = (message)=>console.log(message)
+    socket.on("messageOfServer", recibM(messageS))
+  },[])
   return (
     <div className="App">
-      <h1>hola</h1>
+      <form onSubmit={e=>handleSubmit(e)}>
+        <input type="text" onChange={e=>setMessage(e.target.value)} value={message}/>
+        <button>Send</button>
+      </form>
     </div>
   )
 }
--- index.js
+++ index.js
@@ -9,15 +9,28 @@ const cors = require("cors")
 const SocketServer = Server
 const app = express();
 const server = http.createServer(app)
-const io = new SocketServer(server)
+const io = new SocketServer(server,{
+    cors:{
+        origin: "*"
+    }
+})
 
 //morgan y cors
 app.use(cors())
 app.use(morgan("dev"));
 
+//conectar socket
+io.on("connection",(socket)=>{
+    console.log(`a user connect, id ${socket.id}`)
+    socket.on("messageOfClient", (message)=>{
+        console.log(message)
+        socket.broadcast.emit("messageOfServer",message)
+    })
+})
+
 // configuraciones
 app.set("port", process.env.PORT||3000)
 
 // iniciar server
-app.listen(3000);
+server.listen(3000);
 console.log("server started on port 3000");
